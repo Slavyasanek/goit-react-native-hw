a8d3f5bf06d3e92b3c1367a70bf4752b7ef39dab
Slavyasanek <117304306+Slavyasanek@users.noreply.github.com>
2023-07-12 17:32:42 +0300

    add fonts

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, StatusBar } from 'react-native';
+import { RegistrationScreen } from './src/screens/RegistrationScreen';
+import { useFonts, Roboto_700Bold, Roboto_500Medium } from "@expo-google-fonts/roboto";
 
 export default function App() {
+      const [fontsLoaded] = useFonts({
+        Roboto_700Bold, Roboto_500Medium
+    })
+
+    if (!fontsLoaded) {
+        return null; 
+    }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.container}>
+        <RegistrationScreen />
+        <StatusBar theme="auto" />
+      </View >
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
