c07f50af90f6cf35a092fac73cf1f06f813c4514
Slavyasanek <117304306+Slavyasanek@users.noreply.github.com>
2023-07-19 17:53:56 +0300

    finish 4 hw

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -5,13 +5,14 @@ import { ProfileScreen } from './ProfileScreen'
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { Feather, Ionicons } from "@expo/vector-icons";
 import { StyleSheet } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 const MainTab = createBottomTabNavigator();
 
 export const Home = () => {
+    const navigation = useNavigation();
     return (
         <MainTab.Navigator
             screenOptions={() => ({
-                // headerShown: false,
                 tabBarShowLabel: false,
                 tabBarActiveTintColor: '#FFFFFF',
                 tabBarInactiveTintColor: 'rgba(33, 33, 33, 0.8)',
@@ -27,6 +28,26 @@ export const Home = () => {
                         <TouchableOpacity style={[styles.iconTab, focused && styles.activeIcon]}>
                             <Ionicons name="grid-outline" size={size} color={color} />
                         </TouchableOpacity>
+                    ),
+                    title: 'Публікації',
+                    headerStyle: {
+                        height: 88,
+                        backgroundColor: '#FFFFFF',
+                        borderBottomColor: 'rgba(0, 0, 0, 0.3)',
+                        borderBottomWidth: 0.5,
+                    },
+                    headerTitleAlign: 'center',
+                    headerTintColor: '#212121',
+                    headerTitleStyle: {
+                        fontFamily: 'Roboto_500Medium',
+                        letterSpacing: -0.41,
+                        fontSize: 17,
+                        lineHeight: 22
+                    },
+                    headerRight: () => (
+                        <TouchableOpacity style={styles.logOutIcon} onPress={() => navigation.navigate('Login')}>
+                            <Feather name="log-out" size={24} color={'#BDBDBD'}/>
+                        </TouchableOpacity>
                     )
                 })} />
             <MainTab.Screen name="CreatePosts" component={CreatePostsScreen}
@@ -34,6 +55,29 @@ export const Home = () => {
                     tabBarIcon: ({ focused, size, color }) => (
                         <TouchableOpacity style={[styles.iconTab, focused && styles.activeIcon]}>
                             <Ionicons name="add-outline" size={size} color={color} />
+                        </TouchableOpacity>
+                    ),
+                    tabBarStyle: {
+                        display: 'none',
+                    },
+                    title: 'Створити публікацію',
+                    headerStyle: {
+                        height: 88,
+                        backgroundColor: '#FFFFFF',
+                        borderBottomColor: 'rgba(0, 0, 0, 0.3)',
+                        borderBottomWidth: 0.5,
+                    },
+                    headerTitleAlign: 'center',
+                    headerTintColor: '#212121',
+                    headerTitleStyle: {
+                        fontFamily: 'Roboto_500Medium',
+                        letterSpacing: -0.41,
+                        fontSize: 17,
+                        lineHeight: 22
+                    },
+                    headerLeft: () => (
+                        <TouchableOpacity style={styles.backIcon} onPress={() => navigation.navigate('Posts')}>
+                            <Feather name="arrow-left" size={24} color={'rgba(33, 33, 33, 0.8)'}/>
                         </TouchableOpacity>
                     )
                 })} />
@@ -43,7 +87,8 @@ export const Home = () => {
                                     <TouchableOpacity style={[styles.iconTab, focused && styles.activeIcon]}>
                                         <Feather name="user" size={size} color={color} />
                                     </TouchableOpacity>
-                                )
+                                ),
+                                headerShown: false
                             })} />
         </MainTab.Navigator>
     )
@@ -59,5 +104,11 @@ const styles = StyleSheet.create({
     },
     activeIcon: {
         backgroundColor: '#FF6C00'
+    },
+    logOutIcon: {
+        marginRight: 20
+    },
+    backIcon: {
+        marginLeft: 20
     }
 })
